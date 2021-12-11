d666075eb50f5f5efa1ad4febf12c21a7931367c
wafuwafu13 <mariobaske@i.softbank.jp>
2021-12-11 19:46:42 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#[no_mangle]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+extern {
+    fn date_now() -> f64;
+}
+
+#[no_mangle]
+pub fn get_timestamp() -> f64 {
+    unsafe {
+        date_now()
+    }
+}
